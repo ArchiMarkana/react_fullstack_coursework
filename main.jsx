196a33d972487ffda095c3b85459be66b27765c4
Archi Markana <121239499+ArchiMarkana@users.noreply.github.com>
2025-08-07 08:46:34 +0530

    main.jsx

--- main.jsx
+++ main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Registration from './Registration.jsx';
+import ClassRegistration from './ClassRegistration.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
 
     <Registration />
+    <ClassRegistration />
 
   </StrictMode>,
 );
